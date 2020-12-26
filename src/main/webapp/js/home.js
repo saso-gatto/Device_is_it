fde6f8b7e0c7956a29fe6783d75f7d2ca29d1cc1
kate <katerinagerace@hotmail.it>
2020-12-26 17:13:15 +0100

    aggiunta bottone exit al form registrazione

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function(){ //"load"-->al caricamento della pagina inizializziamo il form per effettuare login e sign up
 	checklogin();
 });
 
@@ -15,67 +15,69 @@ function  checklogin(){
 			$("#first").fadeIn("fast");
 		});
 	});
+}	
+
+ $(function() {
+   $("form[name='login']").validate({
+     rules: {
+       
+       email: {
+         required: true,
+         email: true
+       },
+       password: {
+         required: true,
+         
+       }
+     },
+      messages: {
+       email: "Please enter a valid email address",
+      
+       password: {
+         required: "Please enter password",
+        
+       }
+       
+     },
+     submitHandler: function(form) {
+       form.submit();
+     }
+   });
+ });
+         
+
+
+$(function() {
+  
+  $("form[name='registration']").validate({
+    rules: {
+      firstname: "required",
+      lastname: "required",
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    
+    messages: {
+      firstname: "Please enter your firstname",
+      lastname: "Please enter your lastname",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+  
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
 	
-	 $(function() {
-	   $("form[name='login']").validate({
-	     rules: {
-	       
-	       email: {
-	         required: true,
-	         email: true
-	       },
-	       password: {
-	         required: true,
-	         
-	       }
-	     },
-	      messages: {
-	       email: "Please enter a valid email address",
-	      
-	       password: {
-	         required: "Please enter password",
-	        
-	       }
-	       
-	     },
-	     submitHandler: function(form) {
-	       form.submit();
-	     }
-	   });
-	 });
-	         
-	
-	
-	$(function() {
-	  
-	  $("form[name='registration']").validate({
-	    rules: {
-	      firstname: "required",
-	      lastname: "required",
-	      email: {
-	        required: true,
-	        email: true
-	      },
-	      password: {
-	        required: true,
-	        minlength: 5
-	      }
-	    },
-	    
-	    messages: {
-	      firstname: "Please enter your firstname",
-	      lastname: "Please enter your lastname",
-	      password: {
-	        required: "Please provide a password",
-	        minlength: "Your password must be at least 5 characters long"
-	      },
-	      email: "Please enter a valid email address"
-	    },
-	  
-	    submitHandler: function(form) {
-	      form.submit();
-	    }
-	  });
-	});
-}
+
 
