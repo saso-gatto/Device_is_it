b82eadff03aa8ca56b87cf45344b96f5a5c935cb
Saso <salvatore.gatto99@gmail.com>
2020-12-27 12:30:33 +0100

    "Aggiunta della pagina html relativa alle schede tecniche"

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){ //"load"-->al caricamento della pagina inizializziamo il form per effettuare login e sign up
+	checklogin();
+});
+
+function  checklogin(){
+	 
+	$("#signup").click(function() {	
+		$("#first").fadeOut("fast", function() {
+			$("#second").fadeIn("fast");
+		});
+	});
+	
+	$("#signin").click(function() {
+		$("#second").fadeOut("fast", function() {
+			$("#first").fadeIn("fast");
+		});
+	});
+}
