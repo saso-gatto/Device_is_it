7fb009af638a60a88fe5978be4beb7008370aa87
Debora <you@example.com>
2021-01-08 16:04:24 +0100

    "Delete classe SchedaTecnica & update classe Device"

--- src/main/java/Demo/deviceIsIt/model/Device.java
+++ src/main/java/Demo/deviceIsIt/model/Device.java
@@ -6,7 +6,15 @@ public class Device {
 	private String modello;
 	private String marca;
 	private int tipoDevice;
-	private int idScheda;
+	private String cpu;
+	private String ram;
+	private String display;
+	private String batteria;
+	private String fotocamera;
+	private String memoria;
+	private String peso;
+	private String os;
+	private String img;
 	
 	
 	public int getIdDevice() {
@@ -33,12 +41,61 @@ public class Device {
 	public void setTipoDevice(int tipoDevice) {
 		this.tipoDevice = tipoDevice;
 	}
-	public int getIdScheda() {
-		return idScheda;
+	public String getCpu() {
+		return cpu;
 	}
-	public void setIdScheda(int idScheda) {
-		this.idScheda = idScheda;
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
 	}
+	public String getRam() {
+		return ram;
+	}
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	public String getDisplay() {
+		return display;
+	}
+	public void setDisplay(String display) {
+		this.display = display;
+	}
+	public String getBatteria() {
+		return batteria;
+	}
+	public void setBatteria(String batteria) {
+		this.batteria = batteria;
+	}
+	public String getFotocamera() {
+		return fotocamera;
+	}
+	public void setFotocamera(String fotocamera) {
+		this.fotocamera = fotocamera;
+	}
+	public String getMemoria() {
+		return memoria;
+	}
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+	public String getPeso() {
+		return peso;
+	}
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
+	public String getOs() {
+		return os;
+	}
+	public void setOs(String os) {
+		this.os = os;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
 
 	
 }
