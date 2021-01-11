15b7a1157a415ea80a52ea4a223c085cb5ac4b05
Saso <salvatore.gatto99@gmail.com>
2021-01-11 19:06:16 +0100

    Bozza di ajax + fix dei tasti verdi in news.

---
+++ src/main/webapp/js/commento.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+alert("sono qui");
+/*
+window.addEventListener("load", function(){
+	//inizializzaDati();
+	//popolaTabella();
+	alert("sono qui");
+	registraEventi();	
+});
+
+ 
+ 
+function registraEventi(){
+	var btnCommento = document.getElementById("submitCommento");
+	btnCommento.addEventListener("click", aggiungiCommento);	
+	console.log("sono in registra eventi!"); 
+}
+ 
+ 
+ function aggiungiCommento(){
+	// var bottone = document.querySelector("#btnIscrivi");
+	
+//	var matr = document.querySelector("#matricola").value;	
+	var idcontenuto = document.querySelect("#idcontenuto").value;		
+	var testo = document.querySelector("#testoCommento").value;
+	var data = new Date();
+	var username="prova";
+	
+	$.ajax({ //
+		url: "AggiungiCommento", //URL indica il controller che si 
+		method: "POST",
+		data: {testoCommento: testo, idContenuto=idcontenuto, data=data, username=username},
+		success: function(){
+			
+		},
+		fail: function( jqXHR, textStatus ) {
+  			alert( "Request failed: " + textStatus );
+		}
+	});	
+	
+		
+}*/
