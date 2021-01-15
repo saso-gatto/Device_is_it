2fe35cb9279dbb5c638052eaa96836dfdab507ab
Saso <salvatore.gatto99@gmail.com>
2021-01-15 23:25:41 +0100

    Caricamento di una news dal sito con ajax e JSon

--- src/main/webapp/js/creaContenuto.js
+++ src/main/webapp/js/creaContenuto.js
@@ -9,11 +9,42 @@ function Contenuto(id, data, device, testo, titolo,tipo,img, anteprima){
 	this.anteprima=anteprima;
 }
 
-function SchedaTecnica(){
-}
 
+$(document ).ready(function() {
+    console.log( "ready!" );
+	var pubblicaContenuto=document.getElementById("btnPubblicaContenuto").addEventListener("click",function(){
+		creazioneContenuto();
+		});
+});
 
 function creazioneContenuto(){
-	var id = null;
+	alert("sono in creazione contenuto");
 	var data = new Date();
+	var device =  document.getElementById("deviceDaAggiungere").value;
+	var testo = document.getElementById("testo").value;
+	var titolo = document.getElementById("titoloRec").value;
+	var tipo = document.getElementById("tipoContenuto").value;
+	var img= document.getElementById("img").value;
+	var anteprima=document.getElementById("anteprima").value;
+	var contenuto = new Contenuto (null,data,device,testo,titolo,tipo,img,anteprima);
+	$.ajax({
+			  url: "AggiungiContenuto",  
+	          method: "POST",
+	          // specifico la URL della risorsa da contattare
+	          data: JSON.stringify(contenuto),
+	          // imposto l'azione in caso di successo
+	          contentType: "application/json",
+	          
+	          success: function(risposta){
+	          console.log(JSON.stringify(risposta));
+			  //visualizzo il contenuto del file nel div htmlm
+			  window.location.href="/index";
+	          alert("funziona");
+	          },
+	          //imposto l'azione in caso di insuccesso
+	          fail: function( jqXHR, textStatus ) {
+	  			alert( "Request failed: " + textStatus );
+	          }        
+	    });	
+	
 }
